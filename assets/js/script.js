c6a6019f3535b48fb544c23b495f7f9acf9f1cbc
carolbatista <carolbatista2@gmail.com>
2021-10-21 18:01:34 -0300

    Envio das primeiras funções js adicionando os numeros clicados ao displau e limpando display ao clicar em Clear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
+var x = ''
+var display = document.querySelector('#values')
+var c = document.querySelector('#clean')
 
-var display = document.querySelector('.display')
 
-a.addEventListener('click')
+function numDisplay(n) {
 
-function clicar (){
-    
+    x += n + ' '
+    if(x.length <= 30){
 
+        console.log(x)
+        display.innerHTML = x
+
+    }
+    return x     
+  
+} 
+
+
+function clearX() {
+ 
+    display.innerHTML = ' '
+    return;
+  
 }
-   
-
 
